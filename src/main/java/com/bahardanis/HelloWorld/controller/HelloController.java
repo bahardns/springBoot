4ee946eac627724136d3e898e5a05c3c32ab9f9d
bahardns <bahardns@hotmail.com>
2022-11-13 17:25:01 +0300

    requestMapping anations added class and method levels

--- src/main/java/com/bahardanis/HelloWorld/controller/HelloController.java
+++ src/main/java/com/bahardanis/HelloWorld/controller/HelloController.java
@@ -1,19 +1,25 @@
 package com.bahardanis.HelloWorld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//kontrol sınıfı olduğunu belirtmek için kullanılan anatasyon.
+@RequestMapping(path = "/api")
+//sınıf seviyesinde kullanılan anatasyondur. 
+//kontrol sınıfı olduğunu belirtmek için kullanılan anatasyon.controller altındaki endpointlere erişmek için, api yazılması gerekmektedir
 public class HelloController {
-	//localhost:8080/hello
-	
-	//hello metoduna ulaşmak için @getMapping kullanıyoruz.
-	
+	// localhost:8080/hello
+
+	// hello metoduna ulaşmak için @getMapping kullanıyoruz.
+
 	@GetMapping(path = "/hello")
-	
+
+	@RequestMapping(path = "/hello", method = RequestMethod.GET)
+
 	public String sayHello() {
 		return "hello world";
 	}
-	
+
 }
