07b15290887a2e6187344395415521f39745f5a5
bahardns <bahardns@hotmail.com>
2022-11-14 18:43:35 +0300

    added mapping anations

--- src/main/java/com/bahardanis/HelloWorld/controller/HelloController.java
+++ src/main/java/com/bahardanis/HelloWorld/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.bahardanis.HelloWorld.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,15 @@ public class HelloController {
 	public String sayHello() {
 		return "hello world";
 	}
+	@PostMapping("/update")
+	public String update() {
+		return "data is updated";
+	}
+	
+	@DeleteMapping("/delete")
+	public String delete() {
+		return "data is deleted";
+	}
+	
 
 }
